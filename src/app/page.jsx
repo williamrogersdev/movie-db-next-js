8c0baef659f5a2a249d002fb64676b12cfe623c7
William Rogers <williamrogersdev@gmail.com>
2023-02-16 14:10:49 -0600

    removed files and code  that were not needed for this version if next

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import { Inter } from '@next/font/google'
+import styles from './page.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <h1>Home</h1>
+  )
+}
