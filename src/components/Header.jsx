daee0993c3a35cb2cdd94658cf3363d3f403612a
William Rogers <williamrogersdev@gmail.com>
2023-02-16 15:32:55 -0600

    added menu items

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import MenuItem from "./MenuItem";
+import {AiFillHome} from "react-icons/ai";
+
+
 
 export default function Header() {
   return (
     <div>
-      <div className=""></div>
+      <div className="">
+        <MenuItem title="HOME" address="/" Icon={AiFillHome}/>
+      </div>
       <div className=""></div>
     </div>
   );
