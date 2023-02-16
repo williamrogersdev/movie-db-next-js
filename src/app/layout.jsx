90b2f0d97c37e7677ac8b1fd3c732bd4121aa76f
William Rogers <williamrogersdev@gmail.com>
2023-02-16 15:16:05 -0600

    created header component

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,11 +1,20 @@
-import './globals.css'
+import Header from "@/components/Header";
+import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-    
-    
-      <body>{children}</body>
+      <body>
+        {/* {Header} */}
+
+        <Header />
+
+        {/* {NavBar} */}
+
+        {/* {SearchBox} */}
+
+        {children}
+      </body>
     </html>
-  )
+  );
 }
