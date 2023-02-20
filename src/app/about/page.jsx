bd978edda2dcd412632fbd1cd52fe7f5ee7562f5
William Rogers <williamrogersdev@gmail.com>
2023-02-20 17:44:10 -0600

    finished about us pag

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div className='max-w-6xl mx-auto space-y-4'>
+    <div className='max-w-6xl mx-auto space-y-4 p-4'>
     <h1 className='text-2xl font-medium text-amber-600'>About</h1>
     <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam esse nostrum, mollitia in laudantium amet aperiam aliquid deserunt laborum sunt pariatur quia nihil quo sint repellendus. Iste fugiat porro esse.
     </p>
 
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt veritatis ducimus aspernatur quaerat, ab mollitia, itaque cum temporibus amet beatae esse, saepe expedita. Placeat saepe quod distinctio maxime officia repellendus molestias debitis illum rerum in, rem facilis veniam sequi soluta aut, vero dolores cupiditate voluptatibus explicabo at harum praesentium laborum? In repellat possimus eum atque placeat perspiciatis cumque, quaerat ex consequatur sed vel nihil! Nisi, possimus dignissimos! Consectetur enim dicta excepturi voluptatibus corrupti harum repellendus placeat doloribus, necessitatibus dolor debitis nesciunt ullam nobis quos cum. Eveniet sapiente nobis perspiciatis ab sunt hic exercitationem. Nisi tempore placeat officia cumque natus?</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt veritatis ducimus aspernatur quaerat, ab mollitia, itaque cum temporibus amet beatae esse, saepe expedita. Placeat saepe quod distinctio maxime officia repellendus molestias debitis illum rerum in, rem facilis veniam sequi soluta aut, vero dolores cupiditate voluptatibus explicabo at harum praesentium </p>
+
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt veritatis ducimus aspernatur quaerat, ab mollitia, itaque cum temporibus amet beatae esse, saepe expedita. Placeat saepe quod distinctio maxime officia repellendus molestias debitis illum rerum in, rem facilis veniam sequi soluta ?</p>
+
+
     </div>
   )
 }
