5e620c1bc1db54a7c69329dc8786ae90f613f7fd
William Rogers <williamrogersdev@gmail.com>
2023-02-20 17:57:31 -0600

    added navbar and routes

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,5 @@
 import Header from "@/components/Header";
+import Navbar from "@/components/Navbar";
 import "./globals.css";
 import Providers from "./Providers";
 
@@ -15,6 +16,8 @@ export default function RootLayout({ children }) {
         <Header />
 
         {/* {NavBar} */}
+
+        <Navbar />
 
         {/* {SearchBox} */}
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavbarItem from './NavbarItem'
+
+export default function Navbar() {
+  return (
+    <div className='flex justify-center'>
+<NavbarItem title="Trending" param="fetchTrending" />
+<NavbarItem title="Top Rated" param="fetchTopRated" />
+
+
+    </div>
+  )
+}
