5365d28f228a8b8512a7ca42b8f625f6ccb1fe1e
William Rogers <williamrogersdev@gmail.com>
2023-02-20 18:33:34 -0600

    added movie api and changed home page func to async

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,13 @@
-import { Inter } from '@next/font/google'
+const API_KEY = process.env.API_KEY;
 
-const inter = Inter({ subsets: ['latin'] })
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
 
-export default function Home() {
-  return (
-    <h1 className='text-red-600'>Home</h1>
-  )
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${
+      genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+    }?api_key=${API_KEY}&language=en-US&page=1`
+  );
+
+  return <h1 className="text-red-600">Home</h1>;
 }
