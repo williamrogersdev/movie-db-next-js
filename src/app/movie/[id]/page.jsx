57375f3ef867d3e416bbee4192e63ccc4fe71a96
William Rogers <williamrogersdev@gmail.com>
2023-02-21 16:07:09 -0600

    finished movie listing page

--- src/app/movie/[id]/page.jsx
+++ src/app/movie/[id]/page.jsx
@@ -1,15 +1,12 @@
 import Image from "next/image";
 import React from "react";
 
-
 async function getMovie(movieId) {
   const res = await fetch(
     `https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.API_KEY}`
   );
   return await res.json();
 }
-
-
 
 export default async function MoviePage({ params }) {
   const movieId = params.id;
@@ -18,8 +15,7 @@ export default async function MoviePage({ params }) {
   return (
     <div className="w-full">
       <div className="p-4 md:pt-8 flex flex-col md:flex-row items-center content-center max-w-6xl mx-auto md:space-x-6">
-
-      <Image
+        <Image
           src={`https://image.tmdb.org/t/p/original/${
             movie.backdrop_path || movie.poster_path
           }`}
@@ -35,6 +31,25 @@ export default async function MoviePage({ params }) {
           alt="Movie Poster"
         ></Image>
 
+        <div className="p-2">
+          <h2 className="text-lg mb-3 font-bold">
+            {movie.title || movie.name}
+          </h2>
+
+          <p className="text-lg mb-3">
+            <span className="font-semibold mr-1">Overview: </span>
+
+            {movie.overview}
+          </p>
+          <p className="mb-3">
+            <span className="font-semibold mr-1">Release Date: </span>
+            {movie.release_date || movie.first_air_date}
+          </p>
+          <p className="mb-3">
+            <span className="font-semibold mr-1">Rating: </span>
+            {movie.vote_average}
+          </p>
+        </div>
       </div>
     </div>
   );
