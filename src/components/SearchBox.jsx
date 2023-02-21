335d90e6f4895e0d892d8014f9564cea37bd6fb4
William Rogers <williamrogersdev@gmail.com>
2023-02-21 16:53:26 -0600

    added search functionality

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function SearchBox() {
-  return (
-    <form className='flex max-w-6xl mx-auto justify-between items-center px-5 '>
+  const [search, setSearch] = useState("");
+  const router = useRouter();
 
-        <input type="text" placeholder='Search Movies'
-        className='w-full h-14 rounded-sm placeholder-gray-500 outline-none bg-transparent flex-1'/>
-     <button type='submit' className='text-amber-500 disabled:text-gray-400'> Search </button>
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!search) return;
+
+    router.push(`/search/${search}`);
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="flex max-w-6xl mx-auto justify-between items-center px-5 ">
+      <input
+      value={search}
+      onChange={(e) => setSearch(e.target.value)}
+        type="text"
+        placeholder="Search Movies"
+        className="w-full h-14 rounded-sm placeholder-gray-500 outline-none bg-transparent flex-1"
+      />
+      <button disabled={!search} type="submit" className="text-amber-500 disabled:text-gray-400">
+        {" "}
+        Search{" "}
+      </button>
     </form>
-  )
+  );
 }
