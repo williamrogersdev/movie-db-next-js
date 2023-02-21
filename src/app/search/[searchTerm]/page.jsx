1ab1fdfa6190aaa6a239b934a5d6e2a8843bdd5e
William Rogers <williamrogersdev@gmail.com>
2023-02-21 17:10:00 -0600

    finished search ability and func

--- src/app/search/[searchTerm]/page.jsx
+++ src/app/search/[searchTerm]/page.jsx
@@ -1,6 +1,28 @@
+import Results from "@/components/Results";
 
-export default function SearchPage() {
+export default async function SearchPage({params}) {
+
+
+
+const res = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${params.searchTerm}&language=en-US&page=1`)
+
+
+
+if (!res.ok) {
+    throw new Error("error fetching data")
+  }
+
+  const data = await res.json();
+
+const results = data.results;
+
   return (
-    <div>SearchPage</div>
+    <div>
+
+{results && results.length === 0 && <h1 className="text-2xl text-center">No results found</h1>}
+
+{results && <Results results={results}/>}
+
+    </div>
   )
 }
