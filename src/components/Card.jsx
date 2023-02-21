5ea08f874197611b4b893f2bf7898ad298b3a004
William Rogers <williamrogersdev@gmail.com>
2023-02-21 00:14:16 -0600

    added image config and imported images from movie db

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Card({result}) {
   return (
-    <div className='cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200'>{result.id}</div>
+    <div className='cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200'>
+        <Link href={`/movie/${result.id}`}>
+
+        <Image
+          src={`https://image.tmdb.org/t/p/original/${
+            result.backdrop_path || result.poster_path
+          }`}
+          width={500}
+          height={300}
+          className="sm:rounded-t-lg group-hover:opacity-80 transition-opacity duration-200"
+          style={{
+            maxWidth: "100%",
+            height: "auto",
+          }}
+          placeholder="blur"
+          blurDataURL="/spinner.svg"
+          alt="image is not available"
+        ></Image>
+        </Link>
+        
+        </div>
   )
 }
