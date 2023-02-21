8ddc93337aa7024e9b63a59356c5f69c70821824
William Rogers <williamrogersdev@gmail.com>
2023-02-20 18:55:29 -0600

    added new next js fetch feature for revalidating

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,7 +6,7 @@ export default async function Home({ searchParams }) {
   const res = await fetch(
     `https://api.themoviedb.org/3/${
       genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
-    }?api_key=${API_KEY}&language=en-US&page=1`
+    }?api_key=${API_KEY}&language=en-US&page=1`,{ next: { revalidate: 10000 } }
   );
 
   return <h1 className="text-red-600">Home</h1>;
