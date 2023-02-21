c28af36854fe0f33ce61ccc26661c53db621550a
William Rogers <williamrogersdev@gmail.com>
2023-02-20 23:57:05 -0600

    added the grid system for listing cards

---
+++ src/components/Card.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Card({result}) {
+  return (
+    <div>{result.id}</div>
+  )
+}
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,16 +1,12 @@
-import React from 'react'
+import React from "react";
+import Card from "./Card";
 
-export default function Results({results}) {
+export default function Results({ results }) {
   return (
-    <div>
-
-
-{
-    results.map((result) => (
-        <div key={result.id}>
-            {result.original_title}</div>
-))}
-
+    <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 max-w-6xl mx-auto py-4">
+      {results.map((result) => (
+        <Card key={result.id} result={result} />
+      ))}
     </div>
-  )
+  );
 }
