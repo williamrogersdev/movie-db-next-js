a861aaa74055a14e2f45b9cd1d2209b9dc745d24
William Rogers <williamrogersdev@gmail.com>
2023-02-21 15:56:38 -0600

    added image card to movie page

--- src/app/movie/[id]/page.jsx
+++ src/app/movie/[id]/page.jsx
@@ -1,24 +1,41 @@
+import Image from "next/image";
+import React from "react";
+
+
 async function getMovie(movieId) {
-  const res = await fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.API_KEY}`);
+  const res = await fetch(
+    `https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.API_KEY}`
+  );
   return await res.json();
-
-
 }
 
 
-import React from 'react'
 
-export default async function MoviePage({params}) {
-
-    const movieId = params.id;
-    const movie = await getMovie(movieId);
-
-
+export default async function MoviePage({ params }) {
+  const movieId = params.id;
+  const movie = await getMovie(movieId);
 
   return (
-    <div>
-        
-       <h1>{movie.title || movie.name}</h1>
+    <div className="w-full">
+      <div className="p-4 md:pt-8 flex flex-col md:flex-row items-center content-center max-w-6xl mx-auto md:space-x-6">
+
+      <Image
+          src={`https://image.tmdb.org/t/p/original/${
+            movie.backdrop_path || movie.poster_path
+          }`}
+          width={500}
+          height={300}
+          className="rounded-lg"
+          style={{
+            maxWidth: "100%",
+            height: "100%",
+          }}
+          placeholder="blur"
+          blurDataURL="/spinner.svg"
+          alt="Movie Poster"
+        ></Image>
+
+      </div>
     </div>
-  )
+  );
 }
