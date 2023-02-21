b5e9621c80b678cf0b4ef0446bcc4e4367e3a1bf
William Rogers <williamrogersdev@gmail.com>
2023-02-21 00:34:02 -0600

    added title and realse dates to card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,10 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Card({result}) {
+export default function Card({ result }) {
   return (
-    <div className='cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200 group'>
-        <Link href={`/movie/${result.id}`}>
-
+    <div className="cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200 group">
+      <Link href={`/movie/${result.id}`}>
         <Image
           src={`https://image.tmdb.org/t/p/original/${
             result.backdrop_path || result.poster_path
@@ -21,8 +20,13 @@ export default function Card({result}) {
           blurDataURL="/spinner.svg"
           alt="image is not available"
         ></Image>
-        </Link>
-        
+
+        <div className="p-2">
+          <h2 className="truncate text-lg font-bold">{result.title || result.name}</h2>
+          <p>{result.release_date || result.first_air_date}</p>
+          <p className="line-clamp-4 text-md">{result.overview}</p>
         </div>
-  )
+      </Link>
+    </div>
+  );
 }
